693790e131022572ebe2ecc3862b0e943ff003e2
Gustavo Cesario <luiz.has.gustavo2@gmail.com>
2022-10-28 21:57:33 -0300

    use custom command

---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+
+    it('Deve selecionar produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ajax Full-Zip Sweatshirt')
+            .click()
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 10
+
+
+        cy.get('[class="product-block grid"]')
+            .contains('Ajax Full-Zip Sweatshirt').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+
+    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+       cy.addProdutos('Atlas Fitness Tank', 'M', 'Blue', 3)
+    });
+
+    it.only('Deve adicionar produtos ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Ajax Full-Zip Sweatshirt', 'S', 'Green', 1)
+    });
+
+});
